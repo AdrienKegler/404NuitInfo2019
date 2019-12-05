52dfa9518ee9c1ad3017df530e38e9db0c2fbce8
clement <clement.chabrier@viacesi.fr>
2019-12-05 22:53:45 +0100

    score service local storage

--- nuitInfoErrorPage/src/app/service/score.service.ts
+++ nuitInfoErrorPage/src/app/service/score.service.ts
@@ -7,19 +7,19 @@ export class ScoreService {
   //score: Number = 0
 
   constructor() {
-    if(!sessionStorage.getItem('score')){
-      sessionStorage.setItem('score', '0');
+    if(!localStorage.getItem('score')){
+      localStorage.setItem('score', '0');
     }
    }
 
   incrementScore() {
-    let score = Number(sessionStorage.getItem('score')) + 1
-    sessionStorage.setItem('score', score.toString())
+    let score = Number(localStorage.getItem('score')) + 1
+    localStorage.setItem('score', score.toString())
     //this.score = score
     return score
   }
 
   getScore() {
-    return Number(sessionStorage.getItem('score'))
+    return Number(localStorage.getItem('score'))
   }
 }
